81505df81bd336d5eff5967bb67034a158a1e0d8
johnmdrake1 <32374869+johnmdrake1@users.noreply.github.com>
2024-11-20 20:30:24 -0600

    Added basic bootstrap structure to top level App.js file with a title. Made basic searchbar component with search field and button and bad styling. Added this search bar component to App.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
 
 function SearchBar(){
     return (
         <div>
-            <p>User input for searching songs and search button will go here</p>
+            <Row className='bg-warning'>
+                <Col md={8}>
+                    <input placeholder="Search for a Song" />
+                </Col>
+                <Col md={4}>
+                    <Button variant="success">Search songs</Button>
+                </Col>
+            </Row>
         </div>
     );
 }
