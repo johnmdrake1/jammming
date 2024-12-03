e8b354cf67bb1d97674054e080b8c336801f7f18
johnmdrake1 <32374869+johnmdrake1@users.noreply.github.com>
2024-12-02 19:21:29 -0600

    Added template for spotify object and getAccessToken function in Spotify.js. Added .env file as well with client ID. added .env to .gitignore file.

---
+++ src/utils/Spotify.js
@@ -0,0 +1,15 @@
+//access token value
+let accessToken;
+//replaced with my client id
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+//redirect URI to redirect to after authentification
+const redirectUri = 'http://localhost:3000/';
+
+
+//spotify object with functions for working with the API
+const Spotify = {
+    //Function for getting spotify access token
+    getAccessToken() {
+
+    }
+}
